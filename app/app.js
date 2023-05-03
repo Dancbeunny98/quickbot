1effd3784e60cc9e09c3e0555001fc8ac8b4e3ca
BuildTools <jackworldppooii@gmail.com>
2023-05-03 19:52:44 +0300

    add

--- app/app.js
+++ app/app.js
@@ -1,2 +1,53 @@
 //Основной исполняемый файл бота Discord написанный на node.js.
 //Данный Файл используется для связки функционала бота из папки app и веб-части в папке web с основным index,js в главной дирректории
+
+const express = require('express');
+const { Router } = require('express');
+const { createConnection } = require('mysql2/promise');
+const router = Router();
+const config = require('./config.json');
+
+const app = express();
+
+// Создаем подключение к базе данных MariaDB
+const dbConnection = await createConnection({
+    host: config.db.host,
+    user: config.db.username,
+    password: config.db.password,
+    database: config.db.database,
+    port: config.db.port
+});
+
+// Подключаем маршруты для работы с веб-интерфейсом Discord-Dashboard/Core
+app.use('/dashboard', router);
+
+// Маршрут для получения списка сообщений из базы данных
+router.get('/messages', async (req, res) => {
+    try {
+        const [rows] = await dbConnection.query('SELECT * FROM messages');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Ошибка получения списка сообщений');
+    }
+});
+
+// Маршрут для отправки сообщения в Discord
+router.post('/messages', async (req, res) => {
+    const { content } = req.body;
+
+    try {
+        // Отправляем сообщение в Discord
+        const channel = await client.channels.fetch(config.channelId);
+        const message = await channel.send(content);
+
+        // Добавляем сообщение в базу данных
+        await dbConnection.query('INSERT INTO messages (id, content) VALUES (?, ?)', [message.id, message.content]);
+        res.json(message);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Ошибка отправки сообщения');
+    }
+});
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 //Основной исполняемый файл всего проекта Discord написанный на node.js.
 //Данный Файл используется для связки функционала бота в папке app и веб-части в папке web.
+
+// index.js
+
+const { Client } = require('discord.js');
+const { createConnection } = require('mysql2/promise');
+const app = require('./app');
+const config = require('./config.json');
+
+// Создаем подключение к базе данных MariaDB
+const dbConnection = await createConnection({
+    host: config.db.host,
+    user: config.db.username,
+    password: config.db.password,
+    database: config.db.database,
+    port: config.db.port
+});
+
+// Создаем экземпляр Discord-клиента
+const client = new Client();
+
+// Передаем объект подключения к базе данных в приложение
+app.set('dbConnection', dbConnection);
+
+// Подключаемся к Discord API
+client.login(config.token);
+
+// Обработчик события готовности бота
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+// Обработчик события получения сообщения
+client.on('message', async (message) => {
+    // Проверяем, что сообщение не от бота и начинается с префикса команд (!)
+    if (!message.author.bot && message.content.startsWith(config.prefix)) {
+        const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+
+        // Ищем файл команды в папке commands
+        try {
+            const commandFile = require(`./commands/${command}.js`);
+            // Выполняем команду, передавая ей аргументы и объект сообщения
+            await commandFile.run(client, message, args);
+        } catch (err) {
+            console.error(err);
+            message.reply('Ошибка выполнения команды');
+        }
+    }
+});
